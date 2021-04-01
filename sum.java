b276ef5f7e2051a92f2b47f37017b40c09c891e4
Dahye000 <jin5032192@gmail.com>
2021-04-01 11:08:22 +0900

    1부터 100까지 합

---
+++ sum.java
@@ -0,0 +1,15 @@
+package cap01;
+
+public class sum {
+
+	public static void main(String[] args) {
+		int i = 1; int sum = 0; 
+		for(i = 1; i<=100; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+		
+
+	}
+
+}
